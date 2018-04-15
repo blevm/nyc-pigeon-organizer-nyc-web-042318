5a9b16995b91e44dd31fdd573b8148991eac99e5
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 16:11:02 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,7 @@ pigeons_by_name = Hash.new(0)
     detail_hash.each do |detail_key, detail_array|
       detail_array.each do |name|
         pigeons_by_name.each do |pigeon_name, value_hash|
-          
+
           if name == pigeon_name
             pigeons_by_name[name][pigeon_detail] = []
             pigeons_by_name[name][pigeon_detail] << detail_key.to_s
@@ -28,10 +28,12 @@ pigeons_by_name = Hash.new(0)
     detail_hash.each do |detail_key, detail_array|
       detail_array.each do |name|
         pigeons_by_name.each do |pigeon_name, value_hash|
-          
-        if name == pigeon_name && (pigeons_by_name[name][pigeon_detail]).include?(detail_key) === false
-          (pigeons_by_name[name][pigeon_detail]).push(detail_key)
+
+          if name == pigeon_name && (pigeons_by_name[name][pigeon_detail]).include?(detail_key) === false
+            (pigeons_by_name[name][pigeon_detail]).push(detail_key)
+          end
         end
+      end
     end
   end
   pigeons_by_name

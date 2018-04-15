981d7aa8426104789ac6517f84f1c8f20364528d
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 16:00:18 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+pigeons_by_name = Hash.new(0)
+
+  data.each do |pigeon_detail, detail_hash|
+    #binding.pry
+    detail_hash.each do |detail_key, detail_array|
+      detail_array.each do |name|
+        pigeons_by_name[name] = {}
+      end
+    end
+  end
+  data.each do |pigeon_detail, detail_hash|
+    #binding.pry
+    detail_hash.each do |detail_key, detail_array|
+      detail_array.each do |name|
+        pigeons_by_name.each do |pigeon_name, value_hash|
+          binding.pry
+        if name == pigeon_name
+
+        end
+      end
+      end
+    end
+  end
+  pigeons_by_name
+ end

f1a5ac6f461f3b683cc119c80e69e9478b3b4a4a
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 16:07:09 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,7 +19,7 @@ pigeons_by_name = Hash.new(0)
           #binding.pry
         if name == pigeon_name
           pigeons_by_name[name][pigeon_detail] = []
-          pigeons_by_name[name][pigeon_detail] << detail_key
+          pigeons_by_name[name][pigeon_detail] << detail_key.to_str
         end
       end
       end
